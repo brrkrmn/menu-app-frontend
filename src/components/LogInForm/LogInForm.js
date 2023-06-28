d7aed4b6ebdd13eb00e1bf2cf8d81fc245eb3bca
Onur Temiz <esma.berra.karaman@hotmail.com>
2023-06-28 10:32:44 +0300

    create constants folder

--- src/components/LogInForm/LogInForm.js
+++ src/components/LogInForm/LogInForm.js
@@ -1,9 +1,9 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import SubmitButton from "common/buttons/SubmitButton.js";
-import { userSchema } from "common/constants";
 import FormContainer from "common/forms/FormContainer";
 import PasswordInput from "common/forms/PasswordInput";
 import TextInput from "common/forms/TextInput";
+import { userSchema } from "constants/validations";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { logUserIn } from "reducers/loggedInUserReducer";
